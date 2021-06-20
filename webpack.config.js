e1f9d11c61b5b659f1e16b4c308ad6719a256f22
Strahinja Stamenkovic <Strahinja.s@elfak.rs>
2021-06-20 02:24:16 +0200

    Home page mockup v1

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.ts',
+  devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+}; 
